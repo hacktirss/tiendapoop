23667a6b1bac4b1e8f7ca96bc63c4bb1c783d5af
hacktirss <tirso_520@hotmail.com>
2020-10-19 12:18:55 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="es" xml:lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome Store</title>
+    </head>
+    <body>
+        <div>
+            <h1>PoopCorn Store</h1>
+        </div>
+        <?php
+        // Begin page
+        ?>
+    </body>
+</html>
